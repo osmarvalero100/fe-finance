3c9f2235e65f2182ef1487b343f57e33e39f6244
Osmar Valero <osmarvalero100@gmail.com>
2025-12-28 19:04:58 -0500

    Update base

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/stores/auth'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes: [
     {
       path: '/',
