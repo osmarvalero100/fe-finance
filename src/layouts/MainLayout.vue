2cad6ab3203c6ef2c7f268e48ace29f10039f95e
Osmar Valero <osmarvalero100@gmail.com>
2026-05-03 18:47:38 -0500

    Change category link icon

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -97,7 +97,7 @@ router.afterEach(() => {
             :class="{ 'bg-indigo-50 text-indigo-600 border-r-4 border-indigo-600': $route.name === 'categories' }"
           >
             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
             </svg>
             Categorías
           </router-link>
